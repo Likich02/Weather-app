135348fe1035c29af72c9f7752a92aa24491f23e
Likich02 <124595507+Likich02@users.noreply.github.com>
2024-09-06 18:17:38 +0700

    Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Card } from "./Card";
 import { Input } from "./Input/Index";
 
 function App() {
   return (
     <div className="Main">
       <Input />
+      <div className="CardList">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
